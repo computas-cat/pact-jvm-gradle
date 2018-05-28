943bf57300b02635dc4dc65556cd14913d811c4b
Dervis M <dervis4@gmail.com>
2018-05-28 09:27:35 +0200

    Adding spark main.

--- spark-app/src/main/kotlin/no/dervis/spark/app/Main.kt
+++ spark-app/src/main/kotlin/no/dervis/spark/app/Main.kt
@@ -3,6 +3,9 @@ package no.dervis.spark.app
 
 import spark.Spark.get
 
+data class User(val name: String, val email: String, val id: Int)
+
+
 // vararg args: String
 fun main(args: Array<String>) {
     println("Starting Spark-app.")
