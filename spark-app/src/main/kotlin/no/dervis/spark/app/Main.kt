5305eaecef8be6d6b91a00cbf9f953a751bfea0e
Dervis Mansuroglu <dervis4@gmail.com>
2018-06-01 16:40:49 +0200

    Adding initial todo app

--- spark-app/src/main/kotlin/no/dervis/spark/app/Main.kt
+++ spark-app/src/main/kotlin/no/dervis/spark/app/Main.kt
@@ -1,19 +1,63 @@
 @file:JvmName("Main")
 package no.dervis.spark.app
 
-import spark.Spark.get
+import com.google.gson.GsonBuilder
+import org.eclipse.jetty.http.HttpStatus
+import spark.Spark.*
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.util.*
+import java.util.concurrent.atomic.AtomicInteger
 
-data class ToDo(val id: Int, val description: String, val done: Boolean)
+object Json {
+    private val gson = GsonBuilder().setPrettyPrinting().create()
+    fun <T> fromJson(json: String, t: Class<T>): T = gson.fromJson(json, t)
+    fun <T> toJson(t: T) = gson.toJson(t)
+}
 
-class ToDos {
-    val list = mutableListOf<ToDo>()
+val Id = AtomicInteger()
+
+data class Error(val description: String, val error: Exception? = null)
+
+data class ToDoItem(
+        val id: Int = Id.getAndIncrement(),
+        val description: String,
+        val dueDateTime: Date = Date.from(LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant()),
+        val done: Boolean)
+
+data class ToDoList(
+        val todoList: MutableList<ToDoItem>) {
+
+    fun get(id: Int): ToDoItem? = todoList.getOrNull(id)
+    fun add(toDo: ToDoItem) = todoList.add(toDo)
+    //fun update(id: Int, toDo: ToDoItem) =
 
 }
 
-
-// vararg args: String
 fun main(args: Array<String>) {
-    println("Starting Spark-app.")
-    get("/hello") { req, res -> "Hello World" }
+    println("Starting ToDo Spark-app.")
+    val todos = ToDoList(mutableListOf())
+
+    path("/") {
+        get("") { _, _ ->
+            "ToDo App"
+        }
+    }
+
+    path("/todo") {
+        get("") {_, _ -> Json.toJson(todos) }
+
+        get("/:id") { request, response ->
+            val responseItem = todos.get(request.params(":id").toInt()) ?: Error(description = "Item was not found.")
+            if (responseItem is Error) response.status(HttpStatus.NOT_FOUND_404)
+            Json.toJson(responseItem)
+        }
+
+        post("") { request, response ->
+            todos.add(Json.fromJson(request.body(), ToDoItem::class.java))
+            response.status(201)
+            "ok"
+        }
+    }
 }
 
