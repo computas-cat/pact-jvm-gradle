0dae48f0fa17e2757e41e566cd4bffc6f7a22b0d
dervism <mail@dervis.no>
2018-06-04 00:46:54 +0200

    Adding create and get methods.

--- java-app/src/main/java/no/dervis/java/app/ToDoService.java
+++ java-app/src/main/java/no/dervis/java/app/ToDoService.java
@@ -3,8 +3,10 @@ package no.dervis.java.app;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.http.client.fluent.Request;
 import org.apache.http.client.fluent.Response;
+import org.apache.http.entity.ContentType;
 
 import java.io.IOException;
+import java.time.ZonedDateTime;
 import java.util.List;
 
 public class ToDoService {
@@ -17,7 +19,6 @@ public class ToDoService {
         this.host = host;
     }
 
-
     public List<ToDo> getToDos() throws IOException {
         Response response = Request.Get(host + endpoint).execute();
         TodoList todoList = new ObjectMapper().readValue(response.returnContent().asString(), TodoList.class);
@@ -25,4 +26,23 @@ public class ToDoService {
 
         return todoList.getTodoList();
     }
+
+    public ToDo getToDos(ZonedDateTime from, ZonedDateTime to) {
+
+        return null;
+    }
+
+    public Response createTodo(ToDo toDo) {
+        Response response = null;
+        try {
+            response = Request.Post(host + endpoint).bodyString(
+                    new ObjectMapper().writeValueAsString(toDo), ContentType.APPLICATION_JSON
+            ).execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return response;
+    }
+
 }
