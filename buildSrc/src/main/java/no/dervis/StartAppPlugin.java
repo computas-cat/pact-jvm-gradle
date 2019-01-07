7fa3ea80113773a91cfc5ba4d784425602392a4e
dervism <dervis4@gmail.com>
2019-01-07 15:56:27 +0100

    Adding desc to custom plugin.

--- buildSrc/src/main/java/no/dervis/StartAppPlugin.java
+++ buildSrc/src/main/java/no/dervis/StartAppPlugin.java
@@ -6,7 +6,7 @@ import org.gradle.api.Project;
 public class StartAppPlugin implements Plugin<Project> {
     @Override
     public void apply(Project project) {
-        project.task("hello")
-          .doLast(task -> System.out.println("Hello Gradle!"));
+        project.task("startApp")
+          .doLast(task -> System.out.println("A future plugin to start and stop background processes."));
     }
 }
